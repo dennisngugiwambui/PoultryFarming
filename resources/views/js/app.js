079f12db316482a087baf34f44a999388defae54
Denno <dellyit001@gmail.com>
2024-02-20 22:45:34 -0800

    unversioned files

---
+++ resources/views/js/app.js
@@ -0,0 +1,7 @@
+
+// resources/js/app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleComponent from './components/login';
+
+ReactDOM.render(<ExampleComponent />, document.getElementById('app'));
---
+++ resources/views/js/components/login.js
@@ -0,0 +1,12 @@
+// resources/js/components/ExampleComponent.js
+import React from 'react';
+
+function ExampleComponent() {
+return (
+<div>
+    <h1>Hello, React in Laravel!</h1>
+</div>
+);
+}
+
+export default ExampleComponent;
